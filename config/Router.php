f9f51038622306d82e1bf56c0281874fb9226cd4
AudeL <aude.leissen@gmail.com>
2018-05-07 17:40:59 +0200

    Adding the home page way.

--- config/Router.php
+++ config/Router.php
@@ -2,9 +2,27 @@
 
     namespace HaB\config;
 
+    use HaB\src\controllers\FrontController;
+    use Exception;
+
     class Router {
+
+        private $frontController;
+
+        public function __construct(){
+            $this->frontController = new FrontController();
+        }
 
         public function requestRouter(){
 
+            try{
+                if(isset($_GET['action'])){
+
+                } else{
+                    $this->frontController->welcome();
+                }
+            } catch(Exception $e){
+                echo 'Erreur : ' . $e->getMessage();
+            }
         }
     }
