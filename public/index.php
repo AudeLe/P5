66f03fb11ec0afb941420bc07428900afe3e05cb
AudeL <aude.leissen@gmail.com>
2018-05-07 17:40:35 +0200

    Adding the require for the autoload.

--- public/index.php
+++ public/index.php
@@ -1,11 +1,12 @@
 <?php
     require('../config/dev.php');
-
+    require('../vendor/autoload.php');
     // Start a session
     session_start();
     session_regenerate_id(true);
 
     // Call the autoloader here ?
+    //$loader = require '../vendor/autoload.php';
 
     // Call and execute the Router
     $router = new HaB\config\Router();
