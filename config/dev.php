6e7e304b86481cc8483508b4c05409c0d3b05964
AudeL <aude.leissen@gmail.com>
2018-05-07 14:32:33 +0200

    Modification of the constants allowing the connection to the database.

--- config/dev.php
+++ config/dev.php
@@ -1,6 +1,10 @@
 <?php
 
     // Constants for the database connection
-    const DB_HOST = 'mysql:host=localhost;dbname=books;charset=utf8';
+    const DB_TYPE = 'mysql';
+    const DB_HOSTNAME = 'localhost';
+    const DB_NAME = 'books';
+    const DB_CHARSET = 'utf8';
+    const DB_HOST = DB_TYPE.':host='.DB_NAME.';dbname='.DB_NAME.';charset='.DB_CHARSET.'';
     const DB_USER = 'root';
     const DB_PASS = '';
