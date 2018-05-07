ca82e032f60e216cc782fb8bec621f6c0789a584
AudeL <aude.leissen@gmail.com>
2018-05-07 22:00:29 +0200

    Creation of the registration page.

---
+++ templates/registration.php
@@ -0,0 +1,14 @@
+<?php header('refresh:5; url=../public/index.php'); ?>
+
+<?php $this->title = 'Inscription'; ?>
+
+<div class="row" id="registrationMessage">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <p>
+            Votre inscription a bien été prise en compte.<br />
+            Vous allez être redirigé(e) vers la page d'accueil.<br />
+            Si ce n'est pas le cas, veuillez cliquer sur ce <a href="../public/index.php">lien</a>.<br />
+            Veuillez cliquer sur le bouton "Connexion" afin de vous connecter sur le site.
+        </p>
+    </div>
+</div>
