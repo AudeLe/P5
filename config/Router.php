8a8255fd07883148ae285e0a5c63b0716b8fed3c
AudeL <aude.leissen@gmail.com>
2018-05-07 22:02:02 +0200

    Creation of the registration, connection and logOut paths.

--- config/Router.php
+++ config/Router.php
@@ -3,6 +3,7 @@
     namespace config;
 
     use controllers\FrontController;
+    use controllers\BackController;
     use Exception;
 
     class Router {
@@ -11,14 +12,40 @@
 
         public function __construct(){
             $this->frontController = new FrontController();
+            $this->backController = new BackController();
         }
 
         public function requestRouter(){
 
             try{
                 if(isset($_GET['action'])){
+                    /* ----- CONNECTION RELATED ----- */
+
+                    // Registration on the website
+                    if($_GET['action'] == 'registration'){
+                        if(!empty($_POST['login']) && !empty($_POST['passwordVisitor']) && !empty($_POST['passwordVisitorCheck'])){
+                            $this->backController->registration($_POST['login'], $_POST['passwordVisitor'], $_POST['passwordVisitorCheck']);
+                        } else {
+                            throw new Exception('Impossible d\'enregistrer vos informations.');
+                        }
+                    }
+
+                    // Connection on the website
+                    elseif($_GET['action'] == 'connection'){
+                        if(!empty($_POST['loginConnection']) && !empty($_POST['passwordVisitorConnection'])){
+                            $this->backController->connection($_POST['loginConnection'], $_POST['passwordVisitorConnection']);
+                        } else {
+                            throw new Exception('Impossible de vous identifier.');
+                        }
+                    }
+
+                    // Log out of the website
+                    elseif ($_GET['action'] == 'logOut'){
+                        $this->backController->logOut();
+                    }
 
                 } else{
+                    // Default action
                     $this->frontController->welcome();
                 }
             } catch(Exception $e){
