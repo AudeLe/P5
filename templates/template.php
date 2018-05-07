a7d72f6aa8da243074ac69a679a9f2cff53c0212
AudeL <aude.leissen@gmail.com>
2018-05-07 14:27:33 +0200

    Creation of the template.

---
+++ templates/template.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset = "utf-8" />
+        <title><?= $title ?></title>
+
+        <!-- Meta-tags -->
+
+        <!-- Open Graph data -->
+
+        <!-- Twitter Card data -->
+
+        <!-- IE -->
+
+        <!-- Devices -->
+        <meta name="viewport" content="width = device-width, initial-scale = 1">
+
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="../public/css/style.css"/>
+    </head>
+
+    <body>
+
+        <?= $content ?>
+
+        <!-- JavaScript Files -->
+
+    </body>
+
+    <footer>
+
+    </footer>
+</html>
