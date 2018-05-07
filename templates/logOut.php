835d53aedab6ab6a2511349dc7eff7a168812ccd
AudeL <aude.leissen@gmail.com>
2018-05-07 22:00:15 +0200

    Creation of the log out page.

---
+++ templates/logOut.php
@@ -0,0 +1,12 @@
+<?php header('refresh:5; url= ../public/index.php'); ?>
+
+<?php $this->title = 'Déconnexion' ?>
+
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <p>
+            Vous allez être redirigé(e) vers la page d'accueil.<br/>
+            Si ce n'est pas le cas, veuillez cliquer sur <a href="../public/index.php">lien</a>.
+        </p>
+    </div>
+</div>
