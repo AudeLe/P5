c113b290cdf0d18aaa04ec231ece63d68b00b35d
AudeL <aude.leissen@gmail.com>
2018-05-09 11:46:13 +0200

    Adding the use to access the BookDAO file.

--- src/controllers/FrontController.php
+++ src/controllers/FrontController.php
@@ -3,12 +3,18 @@
     namespace controllers;
 
     use models\View;
+    use DAO\BookDAO;
 
     class FrontController{
+
+        public function __construct(){
+            $this->bookManager = new BookDAO();
+        }
 
         public function welcome(){
 
             $view = new View('home');
             $view->render([]);
         }
+
     }
