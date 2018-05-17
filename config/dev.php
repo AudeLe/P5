27e5f34d568530218bc02d84224085afeaff30a3
AudeL <aude.leissen@gmail.com>
2018-05-17 13:59:21 +0200

    Correction of a typo.

--- config/dev.php
+++ config/dev.php
@@ -5,6 +5,6 @@
     const DB_HOSTNAME = 'localhost';
     const DB_NAME = 'books';
     const DB_CHARSET = 'utf8';
-    const DB_HOST = DB_TYPE.':host='.DB_NAME.';dbname='.DB_NAME.';charset='.DB_CHARSET.'';
+    const DB_HOST = DB_TYPE.':host='.DB_HOSTNAME.';dbname='.DB_NAME.';charset='.DB_CHARSET.'';
     const DB_USER = 'root';
     const DB_PASS = '';
