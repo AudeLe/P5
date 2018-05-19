7771f3596cd2405d142d998764be5b58a8fef46a
AudeL <aude.leissen@gmail.com>
2018-05-19 22:46:25 +0200

    Registering the datas from the book selected.

--- src/DAO/BookDAO.php
+++ src/DAO/BookDAO.php
@@ -1,7 +1,29 @@
 <?php
     namespace DAO;
 
-    class BookDAO{
+    class BookDAO extends DAO {
 
+        public function registerBookDatas($bookTitle, $bookAuthors, $bookPublishedDate, $bookDescription, $bookISBN, $bookNbPages){
+            // Vérifier si l'id de session correspond à l'id du visiteur
+
+            $sql = 'SELECT ISBN FROM bookslist WHERE ISBN = ?';
+            $result = $this->sql($sql, ['$bookISBN']);
+            $row = $result->fetch();
+
+            if ($row){
+                echo 'Vous avez déjà enregistré ce livre.';
+            } else {
+                $sql = 'INSERT INTO bookslist(id_member, title, author, summary, publishingYear, ISBN, nbPages) VALUES(:idMember, :title, :author, :summary, :publishingYear, :ISBN, :nbPages)';
+                $result = $this->sql($sql, [
+                    'idMember' => $_SESSION['id'],
+                    'title' => $bookTitle,
+                    'author' => $bookAuthors,
+                    'summary' => $bookDescription,
+                    'publishingYear' => $bookPublishedDate,
+                    'ISBN' => $bookISBN,
+                    'nbPages' => $bookNbPages
+                ]);
+            }
+        }
 
     }
