943021efd5e9afcfd35c3239dbcaeceb1384b64c
AudeL <aude.leissen@gmail.com>
2018-05-20 19:50:37 +0200

    Modification of a value into a SQL request.

--- src/DAO/MemberDAO.php
+++ src/DAO/MemberDAO.php
@@ -5,30 +5,32 @@
 
     class MemberDAO extends DAO{
         public function getMemberBookList(){
-            $sql = 'SELECT id, author, title, summary, publishingYear, ISBN, nbPages FROM bookslist WHERE id_member = :idMember';
+            $sql = 'SELECT id, author, title, summary, publishingYear, ISBN, nbPages FROM bookslist WHERE id_member = :id_member';
             $result = $this->sql($sql, [
-                'idMember' => $_SESSION['id']
+                'id_member' => $_SESSION['id']
             ]);
-            $row = $result->fetch();
+            //$row = $result->fetch();
 
             $memberBookList = [];
             foreach($result as $row){
                 $bookId = $row['id'];
                 $memberBookList[$bookId] = $this->buildObject($row);
             }
-
+            //var_dump($memberBookList);
+            //die();
             return $memberBookList;
 
         }
 
         private function buildObject(array $row){
-            $bookList = new BookList();
-            $bookList->setAuthor($row['author']);
-            $bookList->setTitle($row['title']);
-            $bookList->setIdMember($row['id_member']);
-            $bookList->setISBN($row['ISBN']);
-            $bookList->setSummary($row['summary']);
-            $bookList->setPublishingYear($row['publishingYear']);
-            $bookList->setNbPages($row['nbPages']);
+            $book = new BookList();
+            $book->setAuthor($row['author']);
+            $book->setTitle($row['title']);
+            //$book->setIdMember($row['id_member']);
+            $book->setISBN($row['ISBN']);
+            $book->setSummary($row['summary']);
+            $book->setPublishingYear($row['publishingYear']);
+            $book->setNbPages($row['nbPages']);
+            return $book;
         }
     }
