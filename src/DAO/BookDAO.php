da5bdd60bb97ba8639e9adade48d25d0e43c6198
AudeL <aude.leissen@gmail.com>
2018-05-21 13:06:29 +0200

    If the book is already registered, you can't registered it twice.

--- src/DAO/BookDAO.php
+++ src/DAO/BookDAO.php
@@ -4,22 +4,13 @@
     class BookDAO extends DAO {
 
         public function registerBookDatas($bookTitle, $bookAuthors, $bookPublishedDate, $bookDescription, $bookISBN, $bookNbPages){
-            // Vérifier si l'id de session correspond à l'id du visiteur
-
-            $sql = 'SELECT id_member FROM bookslist WHERE id_member = :id_member
-                UNION
-                SELECT ISBN FROM bookslist WHERE ISBN = :bookISBN';
+            $sql = 'SELECT ISBN, id_member FROM bookslist WHERE ISBN = :bookISBN';
             $result = $this->sql($sql, [
-                'id_member' => $_SESSION['id'],
                 'bookISBN' => $bookISBN
             ]);
             $row = $result->fetch();
-            $currentISBN = $row['ISBN'];
-            var_dump($row);
-            die();
 
-
-            if ($row['ISBN']){
+            if ($row['id_member'] == $_SESSION['id']){
                 echo 'Vous avez déjà enregistré ce livre.';
                 header('Location: ../public/index.php?action=memberProfile&login='.$_SESSION['login'].'');
             } else {
