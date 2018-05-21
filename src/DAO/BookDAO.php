6de441391a054e263849deacecd2a506ea13facf
AudeL <aude.leissen@gmail.com>
2018-05-21 12:54:22 +0200

    Trying to verify if the book the user wants to register already has been registered for this user.

--- src/DAO/BookDAO.php
+++ src/DAO/BookDAO.php
@@ -6,12 +6,22 @@
         public function registerBookDatas($bookTitle, $bookAuthors, $bookPublishedDate, $bookDescription, $bookISBN, $bookNbPages){
             // Vérifier si l'id de session correspond à l'id du visiteur
 
-            $sql = 'SELECT ISBN FROM bookslist WHERE ISBN = ?';
-            $result = $this->sql($sql, ['$bookISBN']);
+            $sql = 'SELECT id_member FROM bookslist WHERE id_member = :id_member
+                UNION
+                SELECT ISBN FROM bookslist WHERE ISBN = :bookISBN';
+            $result = $this->sql($sql, [
+                'id_member' => $_SESSION['id'],
+                'bookISBN' => $bookISBN
+            ]);
             $row = $result->fetch();
+            $currentISBN = $row['ISBN'];
+            var_dump($row);
+            die();
 
-            if ($row){
+
+            if ($row['ISBN']){
                 echo 'Vous avez déjà enregistré ce livre.';
+                header('Location: ../public/index.php?action=memberProfile&login='.$_SESSION['login'].'');
             } else {
                 $sql = 'INSERT INTO bookslist(id_member, title, author, summary, publishingYear, ISBN, nbPages) VALUES(:idMember, :title, :author, :summary, :publishingYear, :ISBN, :nbPages)';
                 $result = $this->sql($sql, [
@@ -23,7 +33,9 @@
                     'ISBN' => $bookISBN,
                     'nbPages' => $bookNbPages
                 ]);
+                header('Location: ../public/index.php?action=memberProfile&login='.$_SESSION['login'].'');
             }
+
         }
 
     }
