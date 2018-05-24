7aed5197d9af1598bbda0b466608e82c1ef3797e
AudeL <aude.leissen@gmail.com>
2018-05-25 00:10:08 +0200

    Deletion of the call of Twig.

--- public/index.php
+++ public/index.php
@@ -9,11 +9,6 @@
     $loader = require '../vendor/autoload.php';
     $loader->addPsr4('HaB\\', __DIR__);
 
-    /*$appTwig = new config\Services();
-    $appTwig = function($twig){
-        return new Twig();
-    };*/
-
     // Call and execute the Router
     $router = new config\Router();
     $router->requestRouter();
