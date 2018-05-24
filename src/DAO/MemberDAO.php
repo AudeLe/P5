d8cb2b6e73c2150393c7b328b914a2c3dfdea949
AudeL <aude.leissen@gmail.com>
2018-05-25 00:06:03 +0200

    Adding PHPMailer to be used instead of SwiftMailer.

--- src/DAO/MemberDAO.php
+++ src/DAO/MemberDAO.php
@@ -1,10 +1,12 @@
 <?php
     namespace DAO;
 
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\exception;
 
-    use Swift_SmtpTransport;
+    /*use Swift_SmtpTransport;
     use Swift_Mailer;
-    use Swift_Message;
+    use Swift_Message;*/
 
     use models\BookList;
 
@@ -27,27 +29,27 @@
 
         // Verify if a book has been registered
         public function searchBook($id, $ISBN){
-            $sql = 'SELECT id_member, author, title, ISBN FROM bookslist WHERE ISBN = :ISBN';
+            $sql = 'SELECT id_member, author, title, ISBN FROM bookslist WHERE id_member = :id';
             $result = $this->sql($sql, [
-                'ISBN' => $ISBN
+                'id' => $id
             ]);
-            $row = $result->fetch();
 
-            if($row['id_member'] == $id){
-                $message = 'Vous avez déjà enregistré cet ouvrage.';
-            } else {
-                $message = 'Vous n\'avez pas ou n\'avez pas encore enregistré cet ouvrage.';
+            foreach($result as $row){
+                $bookISBN = $row['ISBN'];
+
+                if($bookISBN == $ISBN){
+                    $message = 'Vous avez déjà enregistré cet ouvrage.';
+                } else {
+                    $message = 'Vous n\'avez pas ou n\'avez pas encore enregistré cet ouvrage.';
+                }
             }
 
-            /* FONCTIONNE MAIS PROBLEME AU NIVEAU DE LA REDIRECTION*/
-
-            //header('Location: ../public/index.php?action=memberProfile&login='.$_SESSION['login'].'');
             return $message;
         }
 
         public function reachFriend($login, $loginFriend){
             if($login == $loginFriend){
-                echo 'Vous ne pouvez pas vous envoyer une demande d\'ami(e).';
+                $message =  'Vous ne pouvez pas vous envoyer une demande d\'ami(e).';
             } else {
                 $sql = 'SELECT login, email FROM members WHERE login = :login';
                 $result = $this->sql($sql,[
@@ -58,39 +60,68 @@
                 if($row){
                     $email = $row['email'];
 
-                    //var_dump($email);
-                    //die();
                     $this->sendEmail($email);
-                    echo 'Une demande de partage de liste de livres a été envoyé.';
-                } else{
-                    echo 'Le pseudo indiqué ne correspond à aucun de nos membres.';
+                    $message = 'Une demande de partage de liste de livres a été envoyé.';
+
+                } else {
+
+                    $message = 'Le pseudo indiqué ne correspond à aucun de nos membres.';
                 }
 
-                //return $message;
+                return $message;
             }
+            return $message;
         }
 
 
         public function sendEmail($email){
-            // Create the Transport
-            $smtp_host_ip = gethostbyname('smtp.gmail.com');
-            $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
-                ->setUsername('aude.leissen.dev@gmail.com')
-                ->setPassword('ggyuzcgqmxcbofvy');
+            $mail = new PHPMailer(true);
+            try{
+                // Server settings
+
+                //$mail->SMTPDebug = 2;
+                $mail->isSMTP();
+                $mail->Host = 'auth.smtp.1and1.fr';
+                $mail->SMTPAuth = true;
+                $mail->Username = 'hab@audeleissen.com';
+                $mail->Password = '#mp;W"=2qgLbgL]V?-g';
+                $mail->SMTPSecure = 'tls';
+                $mail->Port = 587;
+
+                // Recipients
+                $mail->setfrom('hab@audeleissen.com', 'Plop');
+                $mail->addAddress($email);
+
+                // Content
+                $mail->isHTML(true);
+                $mail->Subject = 'Here is the subject.';
+                $mail->Body = 'This is the HTML message body <b>in bold !</b>';
+                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients.';
+
+                $mail->send();
+                //echo 'Message has been sent';
+            } catch (Exception $e){
+                echo 'Message could not be sent.';
+                echo 'Mailor Error : ' . $mail->ErrorInfo;
+            }
+
+            // SWIFTMAILER - Create the Transport
+            /*$transport = (new Swift_SmtpTransport('auth.smtp.1and1.fr', 465, 'ssl'))
+                ->setUsername('hab@audeleissen.com')
+                ->setPassword('**************');
 
             // Create theMailer using your created Transport
             $mailer = new Swift_Mailer($transport);
 
             // Create a message
             $message = (new Swift_Message('Demande d\'autorisation de partage de liste de livres'))
-                ->setFrom(['aude.leissen.dev@gmail.com' => 'Plop'])
+                ->setFrom(['hab@audeleissen.com' => 'Plop'])
                 ->setTo($email)
                 ->setBody('Here is the message itself');
 
             // Send the message
-            $result = $mailer->send($message);
-            var_dump($result);
-            die();
+            $result = $mailer->send($message);*/
+
         }
 
 
