ef886d81fbfa368e0b06778e7c2ff278a391ad49
AudeL <aude.leissen@gmail.com>
2018-05-26 16:58:57 +0200

    Adding the checkBookFriend function.

--- src/controllers/BookController.php
+++ src/controllers/BookController.php
@@ -21,4 +21,11 @@
 
             echo $this->twig->render('memberPages/searchBookView.html.twig', array('message' => $message));
         }
+
+        public function checkBookFriend($login, $ISBN, $loginFriend){
+            $message = $this->bookManager->checkBookFriend($ISBN, $loginFriend);
+            $friends = $this->memberManager->reminderFriends($login);
+
+            echo $this->twig->render('memberPages/searchBookFriendView.html.twig', array('message' => $message, 'friends' => $friends));
+        }
     }
