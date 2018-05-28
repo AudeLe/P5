1cc67aaed4eaebc9298d3bc68380402d025c0b98
AudeL <aude.leissen@gmail.com>
2018-05-28 23:34:47 +0200

    Modifiication of the order of the "use" and adding some infos within the constructor.

--- src/controllers/Controller.php
+++ src/controllers/Controller.php
@@ -2,25 +2,43 @@
 
     namespace controllers;
 
+    use DAO\AccountDAO;
+    use DAO\AdministratorDAO;
+    use DAO\BookDAO;
+    use DAO\ConnectionDAO;
+    use DAO\ManagingSharedListDAO;
+    use DAO\MemberDAO;
+
+    use controllers\AccountController;
+
+    // TWIG
     use \Twig_Loader_Filesystem;
     use \Twig_Environment;
     use \Twig_Extension_Debug;
-
-    use DAO\ConnectionDAO;
-    use DAO\BookDAO;
-    use DAO\MemberDAO;
-    use DAO\AdministratorDAO;
 
     class Controller{
 
         protected $twig;
 
+        protected $accountManager;
+        protected $adminManager;
+        protected $bookManager;
+        protected $connectionManager;
+        protected $managingSharedList;
+        protected $memberManager;
+
+        protected $accountController;
+
         public function __construct(){
 
+            $this->accountManager = new AccountDAO();
             $this->adminManager = new AdministratorDAO();
             $this->bookManager = new BookDAO();
             $this->connectionManager = new ConnectionDAO();
+            $this->managingSharedList = new ManagingSharedListDAO();
             $this->memberManager = new MemberDAO();
+
+            $this->accountController = new AccountController();
 
             //Twig configuration
             $loader = new Twig_Loader_Filesystem('../templates');
