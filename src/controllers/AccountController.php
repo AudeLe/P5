9312510cc45250dce704f595da8017b0cd1ceca0
AudeL <aude.leissen@gmail.com>
2018-05-28 23:33:06 +0200

    Modify the access to a function.

--- src/controllers/AccountController.php
+++ src/controllers/AccountController.php
@@ -5,32 +5,32 @@
     class AccountController extends Controller{
         // Modification of the credentials
         public function verifyInformations($login, $password){
-            $this->connectionManager->verifyInformations($login, $password);
+            $this->accountManager->verifyInformations($login, $password);
 
             echo $this->twig->render('commonPages/editInformationsView.html.twig');
         }
 
         // Edit the login
         public function editLogin($login){
-            $this->connectionManager->editLogin($login);
+            $this->accountManager->editLogin($login);
             $this->connectionManager->logOut();
         }
 
         // Edit the password
         public function editPassword($password, $confirmPassword){
-            $this->connectionManager->editPassword($password, $confirmPassword);
+            $this->accountManager->editPassword($password, $confirmPassword);
             $this->connectionManager->logOut();
         }
 
         // Edit the mail
         public function editMail($editMail, $confirmEditMail){
-            $this->connectionManager->editMail($editMail, $confirmEditMail);
+            $this->accountManager->editMail($editMail, $confirmEditMail);
             $this->connectionManager->logOut();
         }
 
         // Delete the account
         public function deleteAccount($id, $login, $password){
-            $this->connectionManager->deleteAccount($id, $login, $password);
+            $this->accountManager->deleteAccount($id, $login, $password);
         }
 
         public function accountPage($login){
