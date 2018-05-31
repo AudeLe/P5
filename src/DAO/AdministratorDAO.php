9182dba6ad577906ed7862743b1f36663f444e2b
AudeL <aude.leissen@gmail.com>
2018-05-31 22:33:22 +0200

    Deletion of PHPMailer. Using PHP's mail() function.

--- src/DAO/AdministratorDAO.php
+++ src/DAO/AdministratorDAO.php
@@ -42,54 +42,58 @@
          * @param $emailSeeker
          * @param $subjectMail
          * @param $bodyMail
-         * @return string
-         */
-        // Send an email to the administrator
-        public function contactAdmin($loginSeeker, $emailSeeker, $subjectMail, $bodyMail){
-
-            $this->sendEmail($loginSeeker, $emailSeeker, $subjectMail, $bodyMail);
-
-            $message = $loginSeeker. ', votre message a été envoyé. Vous receverez une réponse le plus rapidement possible.';
-
-            return $message;
-        }
-
-        /**
-         * @param $loginSeeker
-         * @param $emailSeeker
-         * @param $subjectMail
-         * @param $bodyMail
          */
         // PHPMailer is generating the email with specific settings to be send to the administrator
         public function sendEmail($loginSeeker, $emailSeeker, $subjectMail, $bodyMail){
-            $mail = new PHPMailer(true);
-            try{
-                // Server settings
-                $mail->isSMTP();
-                $mail->Host = 'auth.smtp.1and1.fr';
-                $mail->SMTPAuth = true;
-                $mail->Username = 'hab@audeleissen.com';
-                $mail->Password = '#mp;W"=2qgLbgL]V?-g';
-                $mail->SMTPSecure = 'tls';
-                $mail->Port = 587;
-
-                // Recipients
-                $mail->setfrom($emailSeeker, $loginSeeker);
-                $mail->addAddress('hab@audeleissen.com');
-
-                // Content
-                $mail->isHTML(true);
-                $mail->Subject = $subjectMail;
-                $mail->Body = $bodyMail;
-                $mail->AltBody = $bodyMail;
-
-                $mail->send();
-
-            } catch (Exception $e){
-                $errorMessage = 'Votre message n\'a pas pu être envoyé. Erreur : ' . $mail->ErrorInfo.'';
-                header('Location: ../public/index.php?action=error&errorMessage=' . $errorMessage . '');
+            if(!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,6}$#", $emailSeeker)){
+                $return_line = "\r\n";
+            } else{
+                $return_line = "\n";
             }
 
+            // Creation of the messages with a text format and an HTML one
+            $message_txt = $bodyMail;
+            $message_html = $bodyMail;
 
+            // Creation of the boundary
+            $boundary = "-----=".md5(rand());
+
+            //$to = "hab@audeleissen.com";
+            // Define the subject
+            $subject = $subjectMail;
+
+            // Creation of the email header
+            $headers = 'From: ' . $loginSeeker . ' - ' . $emailSeeker . $return_line;
+            $headers .= 'Reply-To: ' . $emailSeeker;
+            $headers .= 'MIME-Version: 1.0' . $return_line;
+            $headers .= 'X-Priority: 3' . $return_line;
+            $headers .= 'Content-Type: multipart/alternative;' . $return_line . ' boundary= ' . $boundary . $return_line;
+
+            // Creation of the message
+            $message = $return_line . '--' . $boundary . $return_line;
+            // Adding the message with a text format
+            $message .= 'Content-Type: text/plain; charset=\'ISO-8859-1\''.$return_line;
+            $message .= 'Content-Transfer-Encoding: 8bit'.$return_line;
+            $message .= $return_line . $message_txt . $return_line;
+
+            $message .= $return_line . '--' . $boundary .$return_line;
+            // Adding the message with the HTML format
+            $message .= 'Content-Type: text/html; charset=\'ISO-8859-1\''.$return_line;
+            $message .= 'Content-Transfer-Encoding: 8bit'.$return_line;
+            $message .= $return_line . $message_html . $return_line;
+
+            $message .= $return_line . '--' . $boundary . '--' . $return_line;
+            $message .= $return_line . '--' . $boundary . '--' . $return_line;
+
+            mail('hab@audeleissen.com', $subject, $message, $headers);
+            if(mail($emailSeeker, $subject, $message, $headers)){
+                $message = $loginSeeker. ', votre message a été envoyé. Vous receverez une réponse le plus rapidement possible.';
+
+                return $message;
+            } else {
+                $message = $loginSeeker. ', votre message n\'a pas pu être envoyé. Veuillez réessayer ultérieurement.';
+
+                return $message;
+            }
         }
     }
